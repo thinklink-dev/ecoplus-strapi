18c6662895be83099dcd751a1767e2036c488593
Think Link <software@thinklink.com.co>
2021-06-15 22:07:42 -0500

    Initial Strapi Setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '11967367b57e13b2d7544c22eba45c96'),
+    },
+  },
+});
