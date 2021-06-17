fbabed1d87c2cfae220ffeea4d0fd1e43d41baaf
Think Link <software@thinklink.com.co>
2021-06-16 21:56:03 -0500

    Strapi setup ready

--- config/database.js
+++ config/database.js
@@ -11,5 +11,18 @@ module.exports = ({ env }) => ({
         useNullAsDefault: true,
       },
     },
+    mysql: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    }
   },
 });
